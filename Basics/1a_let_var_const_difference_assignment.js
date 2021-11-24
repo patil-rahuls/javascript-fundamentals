ca832fc3de8b88474e93a9b6b29ec7f1bc12b107
rahul.patil <rahul.patil@pepperfry.com>
2021-11-24 18:20:21 +0530

    Basics

--- Basics/1a_let_var_const_difference_assignment.js
+++ Basics/1a_let_var_const_difference_assignment.js
@@ -15,13 +15,13 @@ const c = 4.5;
 c = false;    // Uncaught TypeError: Assignment to constant variable.
 
 // This makes *const* the best choice for declaring variables that don’t need to change.
-// Tt prevents mistakes such as accidental re-assignment.
+// It prevents mistakes such as accidental re-assignment.
 
 // However, for Objects *const* does ALLOW mutation NOT re-assignment.
 // re-assignment not allowed for Objects.
 const myArr = [];
 myArr = [1,2,3]; // Uncaught TypeError: Assignment to constant variable
 
-// Mutation allowed for Objects.
+// Mutation allowed for Objects declared using *const*.
 myArr.push(1);   // This works.
 myArr[9] = 10;   // This works too
