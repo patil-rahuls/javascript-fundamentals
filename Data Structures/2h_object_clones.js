08d7ee4ece0d110826d8c748bd8e05526b9cd4c3
rahul patil <60380578+patil-rahuls@users.noreply.github.com>
2024-04-28 16:03:06 +0530

    Updated

--- Data Structures/2h_object_clones.js
+++ Data Structures/2h_object_clones.js
@@ -10,14 +10,33 @@ const myObj = {
 };
 
 // 1. Shallow Copy - Object.assign
+// Syntax - Object.assign(target, ...sources)
 const shallowCopy = Object.assign({}, myObj);
+// The Object.assign() invokes the getters on the source objects and setters on the target. 
+// It assigns properties only, not copying or defining new properties.
+// IMP - But be aware this is a shallow copy - 'nested objects' are still copied as reference.
+    const x = { name: 'Rahul', dob : { year : 1990 }};
+    const copy = Object.assign({}, myObj);
+    // copy - { name: 'Rahul', dob : { year : 1990 }}
+    // Now, in the 'copy' object, all outer most properties i.e. 'name' in this case,
+    // are an independent copy. And all the properties with nested objects as their values
+    // are references.
+    x.name = 'patil';
+    // x - { name: 'patil', dob : { year : 1990 }}
+    // copy - { name: 'Rahul', dob : { year : 1990 }} // name property is independent.
+
+    x.dob.year = 1991;
+    // x - { name: 'patil', dob : { year : 1991 }}
+    // copy - { name: 'Rahul', dob : { year : 1991 }} // the nested object property gets changed.
 
 // 2. Deep Copy
 const deepCopy = JSON.parse(JSON.stringify(myObj));
 // OR using structuredClone()
 const clone = structuredClone(myObj);
+// IMP - does not clones methods and DOM nodes inside the source object.
 
 // 3. Merge one or more Objects.
+// Using destructuring assignment
 let merged = {...obj1, ...obj2};
 
 // Using Object.assign()
